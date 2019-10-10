bf8934f9e3dcb157065931a1a04a0ca73226d149
Marianne <mariannedery0@gmail.com>
2019-10-10 07:20:39 -0400

    Added the files related to my personnal web page

---
+++ javascript_cv.js
@@ -0,0 +1,38 @@
+//VARIABLES
+
+
+
+//FUNCTIONS
+function button1() {
+    document.getElementById("about").style.display = "none";
+    document.getElementById("exp").style.display = "block";
+}
+
+function button2(){
+    document.getElementById("exp").style.display = "none";
+    document.getElementById("about").style.display = "block";
+}
+
+function button3(){
+    document.getElementById("exp").style.display = "none";
+    document.getElementById("edu").style.display = "block";
+}
+
+function button4(){
+    document.getElementById("edu").style.display = "none";
+    document.getElementById("exp").style.display = "block";
+}
+
+function button5(){
+    document.getElementById("skillset1").style.display = "none";
+    document.getElementById("skillset2").style.display = "block";
+}
+
+function button6(){
+    document.getElementById("skillset2").style.display = "none";
+    document.getElementById("skillset1").style.display = "block";
+}
+
+//OTHERS
+
+
